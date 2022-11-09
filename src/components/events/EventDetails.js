6be0c6becacf7d2c258a4ac87b77aa1093e986d5
stephenrudge <sprock.ryder@gmail.com>
2022-11-08 20:26:48 -0600

    eventDetails

--- src/components/events/EventDetails.js
+++ src/components/events/EventDetails.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 export const EventDetails = () => {
-  return (
-    <>
-      {" "}
-      <div>
-        <h2>Event Details Page</h2>
-      </div>
-    </>
-  );
-};
+const [event, setEvent] = useState({})
+// const [scratchEvent, setScratchEvent] = useState([])
+const {eventId} = useParams()
+useEffect(
+  () => {
+ fetch(`http://localhost:8088/events/${eventId}`)
+     .then (response => response.json())
+     .then ((response) => {
+      const eventArray = response
+         setEvent(eventArray)
+        
+     })
+  },
+  [eventId]
+)
+return <section className="events">
+  <header>{event?.name}</header>
+  <div>Date:{event?.date}</div>
+  <div>Description:{event?.description}</div>
+  <div>
+    <img src ={event.img}></img>
+    </div>
+</section>
+}
