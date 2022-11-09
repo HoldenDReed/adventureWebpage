7dc9bb1502b1960eb9a42aa9e26d2216a91c3f90
unknown <mlwheaton06@gmail.com>
2022-11-08 19:25:27 -0600

    edit employee form functioning

--- src/components/events/EventDetails.js
+++ src/components/events/EventDetails.js
@@ -1,10 +1,23 @@
+import { Link, useParams } from "react-router-dom";
+
 export const EventDetails = () => {
+  const localUser = localStorage.getItem("project_user");
+  const userObject = JSON.parse(localUser);
+
+  const {eventId} = useParams()
+
   return (
     <>
-      {" "}
       <div>
         <h2>Event Details Page</h2>
       </div>
+      {
+        userObject.staff
+        ? <Link className="btn event_edit" to={`/eventEdit/${eventId}`}>
+            Edit
+          </Link>
+        : ""
+      }
     </>
   );
 };
