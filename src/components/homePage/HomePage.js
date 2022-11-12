dab8b9a077c39e5dcf55e6b6e2efceafa5b345f0
Ganesh Babu <ganesh.b@live.com>
2022-11-12 11:29:14 -0500

    stash updates

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,22 +1,30 @@
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
 
 export const HomePage = () => {
   const navigate = useNavigate();
   return (
     <>
-      <section>
-        <div>
-          <Link to={`/eventType/1`}>Exhibits</Link>
+      <section className="home">
+        <Link className="event__link" to={`/eventType/1`}>
+          <div className="event__type">Exhibits</div>
+        </Link>
+
+        <div className="event__type">
+          <Link className="event__link" to={`/eventType/2`}>
+            Activites
+          </Link>
         </div>
-        <div>
-          <Link to={`/eventType/2`}>Activites</Link>
+        <div className="event__type">
+          <Link className="event__link" to={`/eventType/3`}>
+            Camps
+          </Link>
         </div>
-        <div>
-          <Link to={`/eventType/3`}>Camps</Link>
-        </div>
-        <div>
-          <Link to={`/eventType/4`}>Planetarium</Link>
+        <div className="event__type">
+          <Link className="event__link" to={`/eventType/4`}>
+            Planetarium
+          </Link>
         </div>
       </section>
     </>
