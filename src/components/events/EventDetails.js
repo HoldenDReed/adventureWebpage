f5d34ecf8451ee12613b66020466d70bb11286ed
unknown <mlwheaton06@gmail.com>
2022-11-13 15:22:02 -0600

    added functioning like button and remove like button

--- src/components/events/EventDetails.js
+++ src/components/events/EventDetails.js
@@ -1,150 +1,150 @@
-
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Comment } from "../comments/Comment";
+
+
 export const EventDetails = () => {
 
   const localUser = localStorage.getItem("project_user");
   const userObject = JSON.parse(localUser);
   
-const [event, setEvent] = useState({})
-const [comments, updateComments] = useState([])
-const {eventId} = useParams()
+  const [event, setEvent] = useState({})
+  const [comments, updateComments] = useState([])
+  const [users, setUsers] = useState([])
+  const {eventId} = useParams()
 
-useEffect(
-  () => {
- fetch(`http://localhost:8088/events/${eventId}`)
-     .then (response => response.json())
-     .then ((response) => {
-      const eventArray = response
-         setEvent(eventArray)
-        
-     })
-  },
-  [eventId]
+  useEffect(
+    () => {
+      fetch(`http://localhost:8088/events/${eventId}`)
+        .then (response => response.json())
+        .then ((response) => {
+          const eventArray = response
+            setEvent(eventArray)
+        })
+    },
+    [eventId]
   )
   
   useEffect(
     () => {
-   fetch(`http://localhost:8088/comments?_expand=user&eventId=${eventId}`)
-       .then (response => response.json())
-       .then ((response) => {
+      fetch(`http://localhost:8088/comments?_embed=likedComments&_expand=user&eventId=${eventId}`)
+        .then (response => response.json())
+        .then ((response) => {
         const commentArray = response
-           updateComments(commentArray)
+            updateComments(commentArray)
           
-       })
+        })
     },
     [eventId]
   )
 
-    const [newComment, update] = useState({
-      datePosted: "",
-      comment: "",
-      eventId: `${eventId}`
-    });
-  
-    const handleSaveButtonClick = (event) => {
-      event.preventDefault();
+  useEffect(
+    () => {
+      fetch(`http://localhost:8088/users`)
+        .then (response => response.json())
+        .then ((response) => {
+        const usersArray = response
+            setUsers(usersArray)
+          
+        })
+    },
+    []
+  )
 
-      const formattedDateStamp = new Date().toLocaleDateString("en-US", { weekday: "long", year: "numeric", month: "long", day: "numeric" });
-      const formattedTimeStamp = new Date().toLocaleTimeString("en-US");
-      const formattedDateTimeStamp = formattedDateStamp + " " + formattedTimeStamp
+  const [newComment, update] = useState({
+    datePosted: "",
+    comment: "",
+    eventId: `${eventId}`
+  });
   
-      const commentToSendToAPI = {
-        userId: userObject.id,
-        eventId: parseInt(newComment.eventId),
-        datePosted: formattedDateTimeStamp,
-        comment: newComment.comment,
-      };
-  
-      return fetch(`http://localhost:8088/comments`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(commentToSendToAPI),
-      })
-        .then((response) => response.json())
-        .then(() => {
-          window.location.reload(false);
-        });
+  const handleSaveButtonClick = (event) => {
+    event.preventDefault();
+
+    const formattedDateStamp = new Date().toLocaleDateString("en-US", { weekday: "long", year: "numeric", month: "long", day: "numeric" });
+    const formattedTimeStamp = new Date().toLocaleTimeString("en-US");
+    const formattedDateTimeStamp = formattedDateStamp + " " + formattedTimeStamp
+
+    const commentToSendToAPI = {
+      userId: userObject.id,
+      eventId: parseInt(newComment.eventId),
+      datePosted: formattedDateTimeStamp,
+      comment: newComment.comment,
     };
 
+    return fetch(`http://localhost:8088/comments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(commentToSendToAPI),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        window.location.reload(false);
+      });
+  };
 
-return <section className="events">
 
-  <header>{event?.name}</header>
-  {
-        userObject.staff
-        ? <Link className="btn event_edit" to={`/eventEdit/${eventId}`}>
-            Edit
-          </Link>
-        : ""
-      }
-  <div>Date:{event?.date}</div>
-  <div>Description:{event?.description}</div>
-  <div>
-    <img src ={event.img}></img>
-    </div>
+  return <section className="events">
 
-<h4>Comment section</h4>
-    <form>
-    <fieldset>
-        <div className="form-group">
-          <label htmlFor="descriptionBox">Comment:</label>
-          <textarea
-            className="descriptionBox"
-            required
-            autoFocus
-            type="text-area"
-            placeholder="Comment"
-            value={newComment.comment}
-            onChange={(evt) => {
-              const copy = { ...newComment };
-              copy.comment = evt.target.value;
-              update(copy);
-            }}
-          />
-        </div>
-      </fieldset>
-      </form>
-
-    <button
-        onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
-        className="btn btn-primary"
-      >
-        Submit New Comment
-      </button>
-
-    <footer className="comment">
-      <div>
-        {comments.map((comment) => (
-          <div key={`comment--${comment.id}`}>
-            <div>{comment.datePosted}</div>
-            <div>{comment.user.fullName}</div>
-            <div>{comment.comment}</div>
-            {
-              userObject.id === comment.userId
-              ? <button
-                onClick={() => 
-                  {
-                    const deleteComment = async () => {
-                    await fetch(`http://localhost:8088/comments/${comment.id}`, {method: "DELETE"})
-                    window.location.reload(false)
-                  }
-                    deleteComment()
-                  }
-                }
-                className="btn btn-primary"
-              >Delete</button>
-              : ""
-            }
-          </div>
-        ))}
+    <header>{event?.name}</header>
+    {
+          userObject.staff
+          ? <Link className="btn event_edit" to={`/eventEdit/${eventId}`}>
+              Edit
+            </Link>
+          : ""
+        }
+    <div>Date:{event?.date}</div>
+    <div>Description:{event?.description}</div>
+    <div>
+      <img src ={event.img}></img>
       </div>
-    </footer>
-</section>
 
+  <h4>Comment section</h4>
+      <form>
+      <fieldset>
+          <div className="form-group">
+            <label htmlFor="descriptionBox">Comment:</label>
+            <textarea
+              className="descriptionBox"
+              required
+              autoFocus
+              type="text-area"
+              placeholder="Comment"
+              value={newComment.comment}
+              onChange={(evt) => {
+                const copy = { ...newComment };
+                copy.comment = evt.target.value;
+                update(copy);
+              }}
+            />
+          </div>
+        </fieldset>
+        </form>
+
+      <button
+          onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
+          className="btn btn-primary"
+        >
+          Submit New Comment
+        </button>
+
+      <footer className="comment">
+        <div>
+          {comments.map((comment) => (
+            <Comment
+              key={`comment--${comment.id}`}
+              id={comment.id}
+              datePosted={comment.datePosted}
+              userFullName={comment.user.fullName}
+              comment={comment.comment}
+              userId={comment.userId}
+            />
+          ))}
+        </div>
+      </footer>
+  </section>
 }
 
